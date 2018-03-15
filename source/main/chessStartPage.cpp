b30391abd1b5149f028d1b4f9eb7ec8b4cd57b1f
zgreenberg02 <31316241+zgreenberg02@users.noreply.github.com>
2018-03-15 08:57:41 -0600

    Add files via upload

---
+++ source/main/chessStartPage.cpp
@@ -0,0 +1,22 @@
+// by zach greenberg
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <array>
+
+using namespace std;
+
+
+
+int main() {
+  system("clear");
+  cout << "\e[8;24;50t";
+  cout<<"/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\"<< "\n\n";
+  cout<<"                 Welcome to Chess!                "<< "\n\n";
+  cout<<"              Please select an option             "<< "\n\n\n";
+  cout<<"                       Start                      "<< "\n\n";
+  cout<<"                   Instructions                   "<< "\n\n";
+  cout<<"                       Modes                      "<< "\n\n";
+  return 0;
+}
