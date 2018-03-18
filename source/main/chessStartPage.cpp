841fb36b147b72fd3244a287acb6797762946bcc
zgreenberg02 <31316241+zgreenberg02@users.noreply.github.com>
2018-03-18 16:30:48 -0600

    Update chessStartPage.cpp

--- source/main/chessStartPage.cpp
+++ source/main/chessStartPage.cpp
@@ -1,5 +1,6 @@
 // by zach greenberg
 
+
 #include <iostream>
 #include <string>
 #include <cstdio>
@@ -7,16 +8,56 @@
 
 using namespace std;
 
-
+string menuSelect;
+string incorect;
+bool incorectSelect = false;
+bool startMenu = true;
 
 int main() {
-  system("clear");
-  cout << "\e[8;24;50t";
-  cout<<"/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\"<< "\n\n";
-  cout<<"                 Welcome to Chess!                "<< "\n\n";
-  cout<<"              Please select an option             "<< "\n\n\n";
-  cout<<"                       Start                      "<< "\n\n";
-  cout<<"                   Instructions                   "<< "\n\n";
-  cout<<"                       Modes                      "<< "\n\n";
+
+
+  while(startMenu){
+    cout << "\e[8;24;50t"<< endl<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    //  system("clear");
+    if(incorectSelect == false){
+      incorect = "                                                  ";
+    }else{
+      incorect = " Please select a valid menu                       ";
+    }
+
+    //  cout<<"/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\/\\|/\\"<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                 Welcome to Chess!                "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"              Please select an option             "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                       Start                      "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                   Instructions                   "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                       Modes                      "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                       Exit                       "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<"                                                  "<< endl;
+    cout<<incorect<< endl;
+    incorectSelect = false;
+    cout<<" Type the menu you wish to select: ", cin>> menuSelect, cout<< endl;
+    //cout<<"\\/|\\/\\/|\\/\\/|\\/\\/|\\/\\/|\\/\\/|\\/\\/|\\/\\/|\\/\\/|\\/\\/|\\/"<< endl;
+
+
+    if(menuSelect == "Start"|| menuSelect == "start"){
+      cout<<"1";
+    } else if(menuSelect == "Instuctions"|| menuSelect == "instuctions"){
+      cout<<"2";
+    } else if(menuSelect == "Modes"|| menuSelect == "modes"){
+      cout<<"3";
+    } else if(menuSelect == "Exit"|| menuSelect == "exit"){
+      startMenu = false;
+    } else {
+    incorectSelect = true;
+    }
+  }
   return 0;
 }
